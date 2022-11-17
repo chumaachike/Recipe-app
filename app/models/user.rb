de526de5924bdf2e2e056d754c64c75ba3a87a23
Edward Achike <edwardachike@gmail.com>
2022-11-17 09:17:03 +0100

    models completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :foods
+  has_many :recipes
+
+  def recent_foods
+    foods.order(created_at: :desc).first(3)
+  end
+  def recent_recipes
+    recipes.order(created_at: :desc).first(3)
+  end
 end
